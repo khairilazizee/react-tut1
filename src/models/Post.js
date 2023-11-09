79be7f4b18160e88f4515752c653792e1833d5f4
Mohamad khairil azizee bin mohamed @ ayub <khairil.mohamed@mdec.com.my>
2023-11-09 18:39:47 +0800

    create api route

---
+++ src/models/Post.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    short_description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+export default mongoose.model("Post", postSchema);
